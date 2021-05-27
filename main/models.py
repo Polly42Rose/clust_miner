55f432690038b11f34d5193ac4cc45c02371a8ea
Polly <pdtarantsova@edu.hse.ru>
2021-05-27 18:33:28 +0300

    migrations

--- main/models.py
+++ main/models.py
@@ -3,30 +3,30 @@ from django.urls import reverse
 
 
 class User(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
-    login = models.CharField(max_length=10)
-    password = models.CharField(max_length=10)
+    id = models.CharField(max_length=20, primary_key=True)
+    login = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
     class Meta:
         db_table = "users"
 
-
-class Attribute(models.Model):
-    """
-    Model representing a book genre (e.g. Science Fiction, Non Fiction).
-    """
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        """
-        String for representing the Model object (in Admin site etc.)
-        """
-        return self.name
-
+#
+# class Attribute(models.Model):
+#     """
+#     Model representing a book genre (e.g. Science Fiction, Non Fiction).
+#     """
+#     name = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         """
+#         String for representing the Model object (in Admin site etc.)
+#         """
+#         return self.name
+#
 
 class Log(models.Model):
-    user_id = models.CharField("user_id", max_length=10)
-    title = models.CharField("title", max_length=20)
+    user_id = models.CharField("user_id", max_length=100)
+    title = models.CharField("title", max_length=100)
     xes_file = models.FileField("xes_file", upload_to="logs", help_text="Выберите файл")
     n_traces = models.IntegerField("n_traces", null=True)
     attributes = models.TextField("attributes", null=True)
